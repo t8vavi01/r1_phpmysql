49528d4491c78746198d922e1f1a0e46677932cc
Ville Valta <t8vavi01@students.oamk.fi>
2019-02-13 08:45:33 +0200

    VAlikko lisätty

---
+++ menu.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>MySQL-PHP -harjoitus</title>
+  </head>
+  <body>
+    <ul>
+      <li> <a href="show_books.php">Kirjalista</a> </li>
+      <li> <a href="add_books.php">Lisää kirja</a> </li>
+      <li> <a href="update_books.php">Muokkaa kirja</a> </li>
+      <li> <a href="delete_books.php">Poista kirja</a> </li>
+    </ul>
+  </body>
+</html>
